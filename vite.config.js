15aef392d2f6d330dfeef1789fc5a2b48a0e5135
richardsbez <richardsbez.enginner@gmail.com>
2026-03-29 19:13:36 -0300

    lenis update

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    // Se você estiver usando isso, certifique-se que o Tailwind está no postcss.config.js
+    transformer: 'postcss',
+    lightningcss: {
+      // configurações opcionais
+    }
+  }
 })
